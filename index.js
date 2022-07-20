aaeedd3212c04449d7ae9a4ba39a9e6caf05c317
anilkumartl <anilgowdatl@gmail.com>
2022-07-20 07:06:11 +0530

    json sent

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let http = require("http");
+let emp = { name: "anil", age: 20 };
 http
   .createServer((req, res) => {
-    res.end("this is my fist url");
+    res.end(JSON.stringify(emp));
   })
   .listen(process.env.PORT || 5000);
