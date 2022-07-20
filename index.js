a822529fe38820b3f5c7940bbed71629b4ec55fd
anilkumartl <anilgowdatl@gmail.com>
2022-07-20 07:17:38 +0530

    json fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let http = require("http");
 let emp = { name: "anil", age: 20 };
 http
   .createServer((req, res) => {
+    res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify(emp));
   })
   .listen(process.env.PORT || 5000);
